72b356b2846daf8fc025c26c2ada26b8f0c3a3ef
sunqi <sunqi@163.com>
2017-07-10 12:15:26 +0800

    完成教师的列表渲染以及查看教师功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,37 +1,37 @@
-// define(["jquery", "template","bootstrap"], function ($,template) {
-//     $.ajax({
-//         type: "get",
-//         url:"/api/teacher",
-//         dataType:"json",
-//         success:function(data){
-//             console.log(data);
-//             var html=template("teaTemp",data);
-//             $("#teaInfo").html(html);
-//             // 查看讲师信息
-//             previewTeacher();
-//         }
+define(["jquery", "template","bootstrap"], function ($,template) {
+    $.ajax({
+        type: "get",
+        url:"/api/teacher",
+        dataType:"json",
+        success:function(data){
+            console.log(data);
+            var html=template("teaTemp",data);
+            $("#teaInfo").html(html);
+            // 查看讲师信息
+            previewTeacher();
+        }
 
-//     });
+    });
 
 
-//     // 查看讲师功能
-//     function previewTeacher(){
-//         $("#teaInfo").find(".preview").click(function(){
-//             var teaId=$(this).closest("td").data("id");
-//             $.ajax({
-//                 type:"get",
-//                 url:"/api/teacher/view",
-//                 data:{tc_id:teaId},
-//                 dataType:"json",
-//                 success:function(data){
-//                     console.log(data);
-//                     // tc_hometown:"山东省|济南市|章丘市"
-//                     data.result.tc_hometown=data.result.tc_hometown.split("|").join("");
-//                    var html=template("teaModalInfo",data.result);
-//                    $("#teaDetail").html(html);
-//                 }
-//             });
-//             // return false;
-//         });
-//     }
-// });
+    // 查看讲师功能
+    function previewTeacher(){
+        $("#teaInfo").find(".preview").click(function(){
+            var teaId=$(this).closest("td").data("id");
+            $.ajax({
+                type:"get",
+                url:"/api/teacher/view",
+                data:{tc_id:teaId},
+                dataType:"json",
+                success:function(data){
+                    console.log(data);
+                    // tc_hometown:"山东省|济南市|章丘市"
+                    data.result.tc_hometown=data.result.tc_hometown.split("|").join("");
+                   var html=template("teaModalInfo",data.result);
+                   $("#teaDetail").html(html);
+                }
+            });
+            // return false;
+        });
+    }
+});
